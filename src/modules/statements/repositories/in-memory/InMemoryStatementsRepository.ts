1da7e5ff3fd3f8ea2a916d058ddb725427644a62
leosan0 <leowokoti@gmail.com>
2021-09-10 16:51:44 -0300

    Challenge not completed

--- src/modules/statements/repositories/in-memory/InMemoryStatementsRepository.ts
+++ src/modules/statements/repositories/in-memory/InMemoryStatementsRepository.ts
@@ -1,4 +1,4 @@
-import { Statement } from "../../entities/Statement";
+import { OperationType, Statement } from "../../entities/Statement";
 import { ICreateStatementDTO } from "../../useCases/createStatement/ICreateStatementDTO";
 import { IGetBalanceDTO } from "../../useCases/getBalance/IGetBalanceDTO";
 import { IGetStatementOperationDTO } from "../../useCases/getStatementOperation/IGetStatementOperationDTO";
@@ -32,11 +32,12 @@ export class InMemoryStatementsRepository implements IStatementsRepository {
     const statement = this.statements.filter(operation => operation.user_id === user_id);
 
     const balance = statement.reduce((acc, operation) => {
-      if (operation.type === 'deposit') {
+      if (operation.type === OperationType.DEPOSIT) {
         return acc + operation.amount;
-      } else {
+      } else if (operation.type === OperationType.WITHDRAW) {
         return acc - operation.amount;
       }
+      return acc + operation.amount;
     }, 0)
 
     if (with_statement) {
